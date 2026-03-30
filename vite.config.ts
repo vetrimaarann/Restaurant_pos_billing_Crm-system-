86a7d098d9bb73bc74c832429b45403a4f3b5f0d
SunderGanesan <85928145+SunderGanesan@users.noreply.github.com>
2026-03-30 18:02:58 +0530

    chore: add GH Pages workflow, set vite base, and responsive styles

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,7 @@ import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: '/Restaurant_pos_billing_Crm-system-/',
   plugins: [
     react(),
     tailwindcss(),
